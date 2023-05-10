ff72a8b4847f1ed03dc8e11957625b1467001762
unknown <flammens@ctg.com>
2023-05-11 01:18:39 +0200

    chore(page2): add tracking

--- src/components/projector/projector.tsx
+++ src/components/projector/projector.tsx
@@ -1,11 +1,14 @@
 import { Slot, component$} from '@builder.io/qwik';
 export interface ProjectorProps{
     message:string;
+    error:boolean;
 }
 export const Projector = component$((props: ProjectorProps) => {
     return <div>
         <Slot></Slot>
-        <span>{props.message}</span>
+        <span class={
+            [{"text-red-500": props.error}]
+        }>{props.message}</span>
     </div>
 
 });
--- src/routes/page2/index.tsx
+++ src/routes/page2/index.tsx
@@ -1,9 +1,16 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
 import { Projector } from "~/components/projector/projector";
 export default component$(() => {
     const messageSignal = useSignal('');
+    const errorSignal = useSignal(false);
+    useTask$(({track}) => {
+        track(() => messageSignal.value);
+        if(messageSignal.value === 'leKeque'){
+            errorSignal.value = true;
+        }
+    });
     return <div>
         <input type="text" placeholder="try me" onInput$={(e) => {messageSignal.value = (e.target as HTMLInputElement).value}}></input>
-        <Projector message={messageSignal.value}>Your message is: </Projector>
+        <Projector message={messageSignal.value} error={errorSignal.value}>Your message is: </Projector>
     </div>;
 });
